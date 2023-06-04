44a87f106536e751e70d2ccb0889395928f65313
jinwook song <wlsdnr129@naver.com>
2023-06-04 18:43:12 +0900

    progress bar with custom paint

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -12,6 +12,7 @@ class MusicPlayerDetailScreen extends StatefulWidget {
 class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Yeonjae'),
@@ -45,8 +46,63 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
               ),
             ),
           ),
+          const SizedBox(height: 50),
+          CustomPaint(
+            size: Size(size.width - 80, 5),
+            painter: ProgressBar(progressValue: size.width / 2),
+          )
         ],
       ),
     );
+  }
+}
+
+class ProgressBar extends CustomPainter {
+  final double progressValue;
+
+  ProgressBar({required this.progressValue});
+  @override
+  void paint(Canvas canvas, Size size) {
+    // track
+    final trackPaint = Paint()
+      ..color = Colors.grey.shade300
+      ..style = PaintingStyle.fill;
+
+    final RRect trackRect = RRect.fromLTRBR(
+      0,
+      0,
+      size.width,
+      size.height,
+      const Radius.circular(10),
+    );
+
+    canvas.drawRRect(trackRect, trackPaint);
+
+    // progress
+    final progresskPaint = Paint()
+      ..color = Colors.grey.shade500
+      ..style = PaintingStyle.fill;
+
+    final RRect progresskRect = RRect.fromLTRBR(
+      0,
+      0,
+      progressValue,
+      size.height,
+      const Radius.circular(10),
+    );
+
+    canvas.drawRRect(progresskRect, progresskPaint);
+
+    // thumb
+    canvas.drawCircle(
+      Offset(progressValue, size.height / 2),
+      10,
+      progresskPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant ProgressBar oldDelegate) {
+    return false;
   }
 }
